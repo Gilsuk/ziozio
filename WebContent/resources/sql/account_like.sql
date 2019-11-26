f5635ee1a451c7aeff06d7cf164b52d134ca5a2a
user1 <user1@DESKTOP-MQA89TU>
2019-11-26 18:49:15 +0900

    add sql

---
+++ WebContent/resources/sql/account_like.sql
@@ -0,0 +1,28 @@
+DROP TABLE account_like;
+
+CREATE TABLE account_like (
+	account_no NUMBER NOT NULL,
+	cloth_combined_no  NUMBER NOT NULL,
+    
+    -- account, cloth_combined 테이블과 외래키 연결
+	CONSTRAINT account_like_to_account_no_fk
+	FOREIGN KEY ( account_no )
+	REFERENCES account ( account_no ),
+    
+    CONSTRAINT account_like_to_cloth_comb_fk
+	FOREIGN KEY ( cloth_combined_no )
+	REFERENCES cloth_combined ( cloth_combined_no )
+    
+    
+);
+
+INSERT INTO ACCOUNT_LIKE (account_no, cloth_combined_no)
+VALUES (1, 1);
+
+INSERT INTO ACCOUNT_LIKE (account_no, cloth_combined_no)
+VALUES (2, 2);
+
+INSERT INTO ACCOUNT_LIKE (account_no, cloth_combined_no)
+VALUES (3, 3);
+
+SELECT * FROM account_like;
--- WebContent/resources/sql/cloth.sql
+++ WebContent/resources/sql/cloth.sql
@@ -2,9 +2,30 @@ DROP TABLE cloth;
 
 CREATE TABLE cloth (
 	cloth_code NUMBER primary key,	--옷코드(외투,상의,하의)
-	cloth_category_code NUMBER,		--카테고리코드(상의-블라우스, 면티,...)
-	cloth_name VARCHAR2(50),		--옷 이름
-	cloth_description VARCHAR2(255),--옷관련 설명(?)
-	cloth_links VARCHAR2(255),		--연결되는 사이트링크
-	cloth_gender CHAR(1)			--성별(여성,중성,남성)
+	cloth_category_code NUMBER NOT NULL,		--카테고리코드(상의-블라우스, 면티,...)
+	cloth_name VARCHAR2(50) NOT NULL,		--옷 이름
+	cloth_description VARCHAR2(255) NOT NULL,--옷관련 설명(?)
+	cloth_links VARCHAR2(255) NOT NULL,		--연결되는 사이트링크
+	cloth_gender CHAR(1) NOT NULL,			--성별(여성,중성,남성)
+    
+    CONSTRAINT cloth_to_cloth_category_fk
+	FOREIGN KEY ( cloth_category_code )
+	REFERENCES cloth_category ( cloth_category_code ),
+    
+    CONSTRAINT cloth_gender_ck
+	CHECK (cloth_gender IN('N', 'M', 'F'))
+    
+    
 );
+
+
+INSERT INTO CLOTH (cloth_code, cloth_category_code, cloth_name, cloth_description, cloth_links, cloth_gender)
+VALUES (1, 1, 'nameA', 'descriptionA', 'linksA', 'F');
+
+INSERT INTO CLOTH (cloth_code, cloth_category_code, cloth_name, cloth_description, cloth_links, cloth_gender)
+VALUES (2, 2, 'nameB', 'descriptionB', 'linksA', 'M');
+
+INSERT INTO CLOTH (cloth_code, cloth_category_code, cloth_name, cloth_description, cloth_links, cloth_gender)
+VALUES (3, 3, 'nameC', 'descriptionC', 'linksA', 'N');
+
+SELECT * FROM cloth;
