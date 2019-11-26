072d758430f4ea901f9f5ed96783435f1fefe22a
박시영 <placeb@naver.com>
2019-11-26 17:39:23 +0900

    add AccountNotVerifiedException

---
+++ src/ziozio/utils/paramparser/AccountNotVerifiedException.java
@@ -0,0 +1,10 @@
+package ziozio.utils.paramparser;
+
+public class AccountNotVerifiedException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -51030742231065105L;
+
+}
