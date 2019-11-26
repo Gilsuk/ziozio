1f1f4dc1009f3759be5c48cb3b1fafd1cf85196a
박시영 <placeb@naver.com>
2019-11-26 14:44:02 +0900

    join(회원가입) 파라미터 수정

--- src/ziozio/controller/JoinController.java
+++ src/ziozio/controller/JoinController.java
@@ -1,6 +1,7 @@
 package ziozio.controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,40 +9,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import ziozio.dao.impl.JoinDAOImpl;
-import ziozio.dto.User;
 import ziozio.service.face.JoinService;
 import ziozio.service.impl.JoinServiceImpl;
+import ziozio.utils.paramparser.InvalidParamException;
 
 /**
  * Servlet implementation class Join
  */
 @WebServlet("/join")
-public class Join extends HttpServlet {
+public class JoinController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private JoinService joinService = JoinServiceImpl.getInstance();
 	
-	
-	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
 		req.getRequestDispatcher("/WEB-INF/views/join/join.jsp").forward(req, resp);
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
 		req.setCharacterEncoding("UTF-8");
 		
-		
-		User user = joinService.getJoinParam(req);
-		
-		joinService.join(user);
-
-		
-		
-		resp.sendRedirect("/main");
+		try {
+			joinService.join(req);
+		} catch (InvalidParamException e) {
+			// 인풋 파라미터가 유효하지 않을 때 처리(email 값을 이상한 것으로 넘긴다던지...)
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// DB에 저장이 안될 때 처리(email, nick 중복, 등)
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/ziozio/dao/face/JoinDAO.java
+++ src/ziozio/dao/face/JoinDAO.java
@@ -1,26 +1,29 @@
 package ziozio.dao.face;
 
-import ziozio.dto.State;
-import ziozio.dto.User;
+import java.sql.SQLException;
+
+import ziozio.dto.AccountIncludeAuthInfo;
 
 public interface JoinDAO {
 
-	String selectUserpwById(String id);
+//	String selectUserpwById(String id);
+//
+//	Account selectUserById(String id);
+//
+//	State selectUserByUserno(int userno);
+//
+//	
+//	public void insertUser(Account user);
+//
+//	
+//	public int selectNextUserno();
+//	
+	
+//	public int emailCheck(String useremail);
+//	
+//	
+//	public int nickCheck(String usernick);
 
-	User selectUserById(String id);
-
-	State selectUserByUserno(int userno);
-
-	
-	public void insertUser(User user);
-
-	
-	public int selectNextUserno();
-	
-	
-	public int emailCheck(String useremail);
-	
-	
-	public int nickCheck(String usernick);
+	void insert(AccountIncludeAuthInfo account) throws SQLException;
 	
 }
