331a2a2cd469438e98deffdbf37defaf07e9fa7f
gih <gih@DESKTOP-MQA89TU>
2019-11-27 12:06:35 +0900

    현재 위치 및 날씨정보

---
+++ WebContent/Location.java
@@ -0,0 +1,28 @@
+package form;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/location")
+public class Location extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+		
+		req.getRequestDispatcher("/WEB-INF/views/location.jsp").forward(req, resp);
+		
+		
+	}
+	
+	
+
+}
