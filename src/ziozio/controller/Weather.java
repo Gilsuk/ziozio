1518e93f489b8120f12824477d68a0c15a9a84e1
goinho <goinho10@naver.com>
2019-11-29 03:17:04 +0900

    location 수정

---
+++ src/ziozio/controller/Weather.java
@@ -0,0 +1,23 @@
+package ziozio.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/weather")
+public class Weather extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.getRequestDispatcher("/WEB-INF/views/weather.jsp").forward(req, resp);
+		
+	}
+
+}
