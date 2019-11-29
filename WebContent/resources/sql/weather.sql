281fa0b8ee4b9dd52c4f266f8ab0c61b0fe47313
박시영 <placeb@naver.com>
2019-11-29 14:39:58 +0900

    add sqls

--- WebContent/resources/sql/weather.sql
+++ WebContent/resources/sql/weather.sql
@@ -6,13 +6,15 @@ CREATE TABLE weather (
 );
 
 INSERT INTO WEATHER (weather_code, weather_name)
-VALUES (1, 'nameA');
+VALUES (1, 'sunny');
 
 INSERT INTO WEATHER (weather_code, weather_name)
-VALUES (2, 'nameB');
+VALUES (2, 'cloudy');
 
 INSERT INTO WEATHER (weather_code, weather_name)
-VALUES (3, 'nameC');
+VALUES (3, 'rainy');
 
+INSERT INTO WEATHER (weather_code, weather_name)
+VALUES (4, 'snowy');
 
 SELECT * FROM weather;
