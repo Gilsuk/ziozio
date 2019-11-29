d4456d4512bc4afadcc67594614d77f5f20fdb7c
박시영 <placeb@naver.com>
2019-11-29 15:31:00 +0900

    add sql to create weather_info table

--- WebContent/resources/sql/weather_info.sql
+++ WebContent/resources/sql/weather_info.sql
@@ -1,18 +1,18 @@
 DROP TABLE weather_info;
 
 CREATE TABLE weather_info (
-	weather_info_date date DEFAULT SYSDATE NOT NULL PRIMARY KEY,
+	weather_info_date date NOT NULL,
 	location_code NUMBER NOT NULL,
 	weather_code NUMBER NOT NULL,
 	temperature_grade_code NUMBER NOT NULL,
 	weather_info_temperature NUMBER NOT NULL,
 	weather_info_finedust NUMBER NOT NULL,
     
-    CONSTRAINT weather_info_to_location_co_fk
+    CONSTRAINT weather_info_to_location_fk
 	FOREIGN KEY ( location_code )
 	REFERENCES location ( location_code ),
     
-    CONSTRAINT weather_info_to_weather_co_fk
+    CONSTRAINT weather_info_to_weather_fk
 	FOREIGN KEY ( weather_code )
 	REFERENCES weather ( weather_code ),
     
@@ -21,13 +21,11 @@ CREATE TABLE weather_info (
 	REFERENCES temperature_grade ( temperature_grade_code )
 );
 
-INSERT INTO WEATHER_INFO (location_code, weather_code, temperature_grade_code, weather_info_temperature, weather_info_finedust)
-VALUES (1, 1, 1, 1, 1);
+ALTER TABLE weather_info
+ADD PRIMARY KEY (weather_info_date, location_code);
 
-INSERT INTO WEATHER_INFO (location_code, weather_code, temperature_grade_code, weather_info_temperature, weather_info_finedust)
-VALUES (2, 2, 2, 2, 2);
+commit;
 
-INSERT INTO WEATHER_INFO (location_code, weather_code, temperature_grade_code, weather_info_temperature, weather_info_finedust)
-VALUES (3, 3, 3, 3, 3);
+-- 날씨 더미데이터 추가 필요
 
 SELECT * FROM weather_info;
