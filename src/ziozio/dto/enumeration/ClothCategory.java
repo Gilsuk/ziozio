ed10b2da2dfb9588c0dfff10b0b9c6ab0b9ef0e1
박시영 <placeb@naver.com>
2019-12-01 22:38:44 +0900

    fix categories to enum

--- src/ziozio/dto/enumeration/ClothCategory.java
+++ src/ziozio/dto/enumeration/ClothCategory.java
@@ -2,6 +2,23 @@ package ziozio.dto.enumeration;
 
 public enum ClothCategory {
 	
-	HEAD, TOP, PANTS, UMBRELLA, MASK;
+	TOP("top", "상의"), BOTTOM("bottom", "하의"), OUTER("outer", "겉옷")
+	, UMBRELLA("umbrella", "우산"), MASK("mask", "마스크");
 
+	private final String paramvalue;
+	private final String dbvalue;
+
+	ClothCategory(String paramvalue, String dbvalue) {;
+		this.paramvalue = paramvalue;
+		this.dbvalue = dbvalue;
+	}
+	
+	@Override
+	public String toString() {
+		return this.paramvalue;
+	}
+
+	public String getDbValue() {
+		return this.dbvalue;
+	}
 }
