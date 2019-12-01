3955f17055b588e57bbf43f56b528eab378e27b3
박시영 <placeb@naver.com>
2019-12-01 12:04:51 +0900

    change cloth dto fields

--- src/ziozio/service/impl/ClothServiceImpl.java
+++ src/ziozio/service/impl/ClothServiceImpl.java
@@ -4,37 +4,38 @@ import java.util.List;
 
 import ziozio.dto.Account;
 import ziozio.dto.Cloth;
+import ziozio.dto.Paging;
 import ziozio.service.face.ClothService;
 
 public class ClothServiceImpl implements ClothService{
 
 	@Override
-	public List<Cloth> getClothesByWeather(String weather, String category, int count) {
+	public List<Cloth> getClothesByWeather(String weather, String category, Paging paging) {
 		return null;
 	}
 
 	@Override
-	public List<Cloth> getClothesByTemperatureGrade(int tempGrade, String category, int count) {
+	public List<Cloth> getClothesByTemperatureGrade(int tempGrade, String category, Paging paging) {
 		return null;
 	}
 
 	@Override
-	public List<Cloth> getClothesByStyle(String style, String category, int count) {
+	public List<Cloth> getClothesByStyle(String style, String category, Paging paging) {
 		return null;
 	}
 
 	@Override
-	public List<Cloth> getClothesByGender(char gender, String category, int count) {
+	public List<Cloth> getClothesByGender(char gender, String category, Paging paging) {
 		return null;
 	}
 
 	@Override
-	public List<Cloth> getClothesByAccountLibrary(Account account, String category, int count) {
+	public List<Cloth> getClothesByAccountLibrary(Account account, String category, Paging paging) {
 		return null;
 	}
 
 	@Override
-	public List<List<Cloth>> getClothSetsByAccountLike(Account account, int count) {
+	public List<List<Cloth>> getClothSetsByAccountLike(Account account, Paging paging) {
 		return null;
 	}
 
