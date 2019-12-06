3406b4d6ba50f646d8f046cdf9df8fdf0f8b9d1a
user1 <user1@DESKTOP-MQA89TU>
2019-12-06 15:46:57 +0900

    weather jsp ing

--- src/ziozio/controller/Weather_get.java
+++ src/ziozio/controller/Weather_get.java
@@ -1,6 +1,7 @@
 package ziozio.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,16 +9,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ziozio.dto.Location;
+import ziozio.dto.WeatherInfo;
+import ziozio.service.face.LocationService;
+import ziozio.service.face.WeatherInfoService;
+import ziozio.service.impl.LocationServiceImpl;
+import ziozio.service.impl.WeatherInfoServiceImpl;
+
 
 @WebServlet("/weather_get")
 public class Weather_get extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
+	private WeatherInfoService weatherinfoService = WeatherInfoServiceImpl.getInstance();
+	private LocationService locationService = LocationServiceImpl.getInstance();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		System.out.println(req.getParameter("temp"));
-		System.out.println(req.getParameter("weather"));
+		
+//		Location location = null;
+		Location location = locationService.getLocation(req);
+		System.out.println(location);
+		List<WeatherInfo> locweatherList = weatherinfoService.getWeatherInfosToday(location);
+		System.out.println(locweatherList);
+		req.setAttribute("locweatherList", locweatherList);
+		
+		req.getRequestDispatcher("/WEB-INF/views/weather/locationweather.jsp").forward(req, resp);
+
 		
 	}
 
--- src/ziozio/dto/WeatherInfo.java
+++ src/ziozio/dto/WeatherInfo.java
@@ -9,13 +9,26 @@ public class WeatherInfo implements DTO {
 	private int temperature_grade_code;		//온도 등급
 	private double weather_info_temperature;//숫자로 표시되는 온도(섭씨)
 	private double weather_info_finedust;	//미세먼지 농도
+	
+	private String weahter_info_date_str;	//날짜, 시간 (JSP에서 출력하기 위해 추가됨)
+	
+	public String getWeahter_info_date_str() {
+		return weahter_info_date_str;
+	}
+	public void setWeahter_info_date_str(String weahter_info_date_str) {
+		this.weahter_info_date_str = weahter_info_date_str;
+	}
+
+	
+	
 	@Override
 	public String toString() {
 		return "WeatherInfo [weahter_info_date=" + weahter_info_date + ", location_name=" + location_name
 				+ ", weather_name=" + weather_name + ", temperature_grade_code=" + temperature_grade_code
 				+ ", weather_info_temperature=" + weather_info_temperature + ", weather_info_finedust="
-				+ weather_info_finedust + "]";
+				+ weather_info_finedust + ", weahter_info_date_str=" + weahter_info_date_str + "]";
 	}
+	
 	public Date getWeahter_info_date() {
 		return weahter_info_date;
 	}
