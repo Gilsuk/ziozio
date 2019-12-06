bac601c7237aa9a6a80b4bfd4356d45d634e613b
박시영 <placeb@naver.com>
2019-12-06 14:50:16 +0900

    add sql to build verification table

---
+++ WebContent/resources/sql/verification.sql
@@ -0,0 +1,17 @@
+DROP TABLE verification CASCADE CONSTRAINT;
+
+CREATE TABLE verification (
+    verification_type CHAR(1),
+    account_no NUMBER,
+    verification_code CHAR(64) NOT NULL,
+    verification_date DATE DEFAULT sysdate NOT NULL,
+	CONSTRAINT verification_type_ck
+	CHECK (verification_type IN('M', 'F'))
+);
+
+ALTER TABLE verification
+ADD PRIMARY KEY (verification_type, account_no);
+
+ALTER TABLE verification
+ADD FOREIGN KEY (account_no)
+REFERENCES account (account_no);
